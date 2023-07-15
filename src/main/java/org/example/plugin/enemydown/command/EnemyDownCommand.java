1c6df2c8b918ba70288a9130d021f3feeb99326c
yuto <yuto@yutonoMacBook-Air.local>
2023-07-16 01:08:41 +0900

    敵によって点数を可変

--- src/main/java/org/example/plugin/enemydown/command/EnemyDownCommand.java
+++ src/main/java/org/example/plugin/enemydown/command/EnemyDownCommand.java
@@ -14,6 +14,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDeathEvent;
@@ -95,14 +96,21 @@ public class EnemyDownCommand implements CommandExecutor, Listener, org.bukkit.e
 
   @EventHandler
   public void onEnemyDeath(EntityDeathEvent e) {
-    Player player = e.getEntity().getKiller();
+    LivingEntity enemy = e.getEntity();
+    Player player = enemy.getKiller();
     if (Objects.isNull(player) || playerScoreList.isEmpty()) {
       return;
     }
 
     for (PlayerScore playerScore : playerScoreList) {
       if (playerScore.getPlayerName().equals(player.getName())) {
-        playerScore.setScore(playerScore.getScore() + 10);
+        int point = switch (enemy.getType()) {
+          case ZOMBIE -> 10;
+          case SKELETON, WITCH -> 20;
+          default -> 0;
+        };
+
+        playerScore.setScore(playerScore.getScore() + point);
         player.sendMessage("敵を倒した！現在のスコアは" + playerScore.getScore() + "点");
       }
     }
@@ -147,7 +155,7 @@ public class EnemyDownCommand implements CommandExecutor, Listener, org.bukkit.e
    * @return　敵
    */
   private EntityType getEnemy() {
-    List<EntityType> enemyList = List.of(EntityType.ZOMBIE,EntityType.SKELETON);
-    return enemyList.get(new SplittableRandom().nextInt(2));
+    List<EntityType> enemyList = List.of(EntityType.ZOMBIE,EntityType.SKELETON,EntityType.WITCH);
+    return enemyList.get(new SplittableRandom().nextInt(enemyList.size()));
   }
 }
